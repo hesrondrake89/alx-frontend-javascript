b7fda8109fa6686af87fbaa4880efc55135f1673
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-08-01 11:39:10 +0000

    tasks

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,7 @@
-const getListStudents = () => {
-
-  const students = [
+export default function getListStudents() {
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return students;
-};
-
-export default getListStudents;
+}
