6d10de0ea4461bc37cc760883f1cf765aee7653d
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-08-01 12:08:17 +0000

    tasks

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,22 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr
-    .filter(student => student.location === city)
-    .map(student => {
-      let grade = newGrades.find(g => g.studentId === student.id);
-      return { ...student, grade: (grade && grade.grade) || 'N/A' };
-    });
-}
+const updateStudentGradeByCity = (students, city, newGrades) => {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
 
+  const studentByCity = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grades = newGrades.filter((note) => student.id === note.studentId);
+      let grade = 'N/A';
+
+      if (grades[0]) {
+        grade = grades[0].grade;
+      }
+
+      return { ...student, grade };
+    });
+
+  return studentByCity;
+};
+
+export default updateStudentGradeByCity;
